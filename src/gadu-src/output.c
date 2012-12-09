e86e8202fe66cbf10d1f99551556ff792f7d05e2
Steve Seltzer <pgp@git-annex.mysteryvortex.com>
2012-12-09 13:34:08 +0100

    gadu: "human readable" now prints on decimal place on values < 10

--- src/gadu-src/output.c
+++ src/gadu-src/output.c
@@ -37,18 +37,32 @@ void prettyprintsize(mpz_t size){
 
   /* start out at 1024, we'll divide by this before using the mulitplier */
   mpz_init_set_ui(tmp,1024);
+  /* while the multiplier is smaller than the value */
   while(i<strlen(MULTIPLIERS) && mpz_cmp(size,tmp)>=0){
-    mpz_mul_ui(tmp,tmp,1024);
+    mpz_mul_ui(tmp,tmp,1024); /* try the next multiplier */
     i++;
   }
   if(i<=0) /* special case for bytes (no multiplier) */
     mpz_out_str(stdout,10,size);
   else{
+    /* go back to the largest multiplier smaller than the value */
     mpz_fdiv_q_ui(tmp,tmp,1024);
     i--;
-    mpz_cdiv_q(tmp,size,tmp); /* out=size/multiplier (rounded up) */
-    mpz_out_str(stdout,10,tmp); /* print the digits */
-    putchar(multipliers[i]); /* print the multiplier char */
+    mpz_mul_ui(size,size,10); /* multiply by 10 for 1 decimal place */
+    mpz_cdiv_q(tmp,size,tmp); /* tmp=size/multiplier (rounded up) */
+    mpz_fdiv_q_ui(size,size,10); /* put the size back */
+
+    /* if value is less than 10, print the decimal */
+    if(mpz_cmp_ui(tmp,100)<0){
+      unsigned long out;
+      out=mpz_get_ui(tmp);
+      printf("%lu.%lu%c",out/10,out%10,multipliers[i]);
+    }
+    else{ /* divide of the decimal and print the value */
+      mpz_cdiv_q_ui(tmp,tmp,10);
+      mpz_out_str(stdout,10,tmp); /* print the digits */
+      putchar(multipliers[i]); /* print the multiplier char */
+    }
   }
   mpz_clear(tmp);
 }
